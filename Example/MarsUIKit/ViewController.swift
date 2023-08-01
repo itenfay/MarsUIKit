ad293ba5fef958b155c7f326f5625bdc5ff50744
chenxing <chenxing640@foxmail.com>
2023-08-01 10:58:03 +0800

    first commit

---
+++ Example/MarsUIKit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MarsUIKit
+//
+//  Created by chenxing on 07/31/2023.
+//  Copyright (c) 2023 chenxing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
